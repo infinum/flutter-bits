f3c72ecbe2cd362534074268fee9e34bb7ff8cf5
Josip Krnjic <itsJoKr@users.noreply.github.com>
2021-06-04 11:42:27 +0200

    Update firebase_log_printer.dart

--- loggy_crashlytics/firebase_log_printer.dart
+++ loggy_crashlytics/firebase_log_printer.dart
@@ -16,7 +16,7 @@ class FirebaseLogPrinter extends LogPrinter {
     }
 
     if (record.level.priority >= LogLevel.error.priority) {
-      FlavorConfig.submitError(record.error, stackTrace: record.stackTrace);
+      FlavorConfig.submitError(record.error, message: record.message, stackTrace: record.stackTrace);
     } else {
       final time = record.time.toIso8601String().split('T')[1];
       final callerFrame = record.callerFrame == null ? '-' : '(${record.callerFrame.location})';
