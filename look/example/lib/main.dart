67a7b1bee05189a186e1219c0c20fde0decb59e2
Dario Kurecki <dario.kurecki@infinum.com>
2022-07-21 14:15:12 +0200

    Converted look to mason brick

--- look/example/lib/main.dart
+++ look/example/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:look/ui/common/look/look_data/look_data.dart';
-import 'package:look/ui/common/look/widget/look_builder.dart';
 
+import 'ui/common/look/look_data/look_data.dart';
 import 'ui/common/look/mapping/theme_data_mapping/theme_data_mapper.dart';
 import 'ui/common/look/widget/look.dart';
+import 'ui/common/look/widget/look_builder.dart';
 import 'ui/home/home_screen.dart';
 
 void main() {
